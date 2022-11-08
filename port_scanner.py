aaabdddd140dc1cd605c33b309aa9d1632a403ee
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-08 22:35:21 +0300

    Update on the banner.

--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,14 @@
+#############################################################################
+## 									   ##
+##	Title : Port Scanner						   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+
 import sockets
+
+socket = sockets.sockets(AF_inet, Sock.Stream)
+
